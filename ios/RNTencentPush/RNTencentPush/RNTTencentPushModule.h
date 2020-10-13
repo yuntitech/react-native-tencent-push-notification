e07a82ced0b3755ad45ebdb4fac8490f09df5efa
leejunhui <leejunhui2015@gmail.com>
2020-10-13 11:25:49 +0800

    feat: add iOS package

---
+++ ios/RNTencentPush/RNTencentPush/RNTTencentPushModule.h
@@ -0,0 +1,17 @@
+//
+//  RNTTencentPushModule.h
+//  RNTencentPush
+//
+//  Created by leejunhui on 2020/10/13.
+//
+
+#import <React/RCTEventEmitter.h>
+#import <React/RCTBridgeModule.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RNTTencentPushModule : RCTEventEmitter
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/RNTencentPush/RNTencentPush/RNTTencentPushModule.m
@@ -0,0 +1,12 @@
+//
+//  RNTTencentPushModule.m
+//  RNTencentPush
+//
+//  Created by leejunhui on 2020/10/13.
+//
+
+#import "RNTTencentPushModule.h"
+
+@implementation RNTTencentPushModule
+
+@end
